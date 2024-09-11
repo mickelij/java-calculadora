ad5a56b9d78451b167ad78ff4752a28b78921df1
mickelij <161876913+mickelij@users.noreply.github.com>
2024-09-11 15:28:03 -0300

    aula.js

---
+++ aula.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 2;
+
+let soma = a + b;
+let sub = a - b; 
+let div = a/ b;
+let mult = a * b;
+
+console.log(soma);
+console.log(sub);
+console.log(div);
+console.log(mult);
+document.write(mult);
